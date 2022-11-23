270e2ba40cecc468a7a0ce3a2f79a84ff4929761
instabot <35345644+instabot42@users.noreply.github.com>
2022-11-23 09:25:29 +0000

    Added some facts and something to ensure dead facts are removed

--- src/facts-feed.js
+++ src/facts-feed.js
@@ -48,16 +48,33 @@ export default class FactsFeed {
     ////////////////////////////////////////////////////
 
     async updateFacts() {
+        // Start by removing all existing facts (so we can delete stuff we no longer want)
+        await this.removeExisting()
+        
+        // Finally add all the current facts in
         logger.info(`Ensuring ${this.config.facts.length} facts are in the drive...`)
         for (const fact of this.config.facts) {
             await this.ensureFact(fact)
         }
     }
 
+    removeExisting() {
+        return new Promise(async (resolve, reject) => {
+            const drive = await this.feedStorage._drive(this.driveId)
+            const d = drive.readdir(Feeds.FEED_PREFIX)
+            d.on('data', async (filename) => {
+                const key = path.join(Feeds.FEED_PREFIX, filename)
+                await this.feedStorage.deleteFile(this.driveId, key)
+            })
+            d.on('end', () => resolve())    
+        })
+    }
+
     async ensureFact(fact) {
         // Generate a filename for the headline
-        const regex = /[^a-z0-9]+/gi;
-        const filename = fact.toLowerCase().trim().replace(regex, '-')
+        const regex = /[^a-z0-9]+/gi
+        const trailing = /-+$/
+        const filename = fact.toLowerCase().trim().replace(regex, '-').replace(trailing, '')
         const key = path.join(Feeds.FEED_PREFIX, filename)
 
         // Ensure that the file exists and is up to date
